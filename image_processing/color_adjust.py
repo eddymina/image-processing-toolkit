54d09cef14413e13eae538225acc48f72d3ced85
Eddy Mina <eddymina@Eddys-MBP-5.fios-router.home>
2019-08-06 14:38:33 -0400

    ca spacing

--- image_processing/color_adjust.py
+++ image_processing/color_adjust.py
@@ -19,9 +19,9 @@ def rgb2gray(rgb):
 	"""
 	Take RGB and convert to Gray 
 	"""
-    r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
-    gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-    return gray
+	r, g, b = rgb[:,:,0], rgb[:,:,1], rgb[:,:,2]
+	gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
+	return gray
 
 def rgb2bgr(RGB):
 	"""
@@ -75,44 +75,44 @@ class intensity_plot:
 	"""
 	Create Image Intensity Plot 
 	"""
-    def __init__(self,img,resize=.5,cmap='plasma'):
-        """
-        Create Intensity Plot Class 
+	def __init__(self,img,resize=.5,cmap='plasma'):
+	    """
+	    Create Intensity Plot Class 
 
-        """
-        if len(img.shape) > 2:
-            img = rgb2gray(img)
-            print("Image must be 2D grey scale... Gray Scaling...")
-        # create the x and y coordinate arrays (here we just use pixel indices)
-        self.img= img 
-        self.resize = resize
-        self.cmap=cmap
-        if self.resize and sum(img.shape)>250:
-            self.img = self.__resize()
-            print('Resizing {}....... New Shape'.format(img.shape),self.img.shape)
-        elif sum(img.shape)<250:
-        	print('Image too small to Resize ') 
-        self.xx, self.yy = np.mgrid[0:self.img.shape[0], 0:self.img.shape[1]]
+	    """
+	    if len(img.shape) > 2:
+	        img = rgb2gray(img)
+	        print("Image must be 2D grey scale... Gray Scaling...")
+	    # create the x and y coordinate arrays (here we just use pixel indices)
+	    self.img= img 
+	    self.resize = resize
+	    self.cmap=cmap
+	    if self.resize and sum(img.shape)>250:
+	        self.img = self.__resize()
+	        print('Resizing {}....... New Shape'.format(img.shape),self.img.shape)
+	    elif sum(img.shape)<250:
+	    	print('Image too small to Resize ') 
+	    self.xx, self.yy = np.mgrid[0:self.img.shape[0], 0:self.img.shape[1]]
         
-    def __resize(self, inter = cv2.INTER_AREA):
-        # initialize the dimensions of the image to be resized and grab the image size
-        dim = tuple([int(self.resize * i) for i in self.img.shape[0:2]])
-        # resize the image
-        return cv2.resize(self.img, (dim[1],dim[0]), interpolation = inter)
+	def __resize(self, inter = cv2.INTER_AREA):
+	    # initialize the dimensions of the image to be resized and grab the image size
+	    dim = tuple([int(self.resize * i) for i in self.img.shape[0:2]])
+	    # resize the image
+	    return cv2.resize(self.img, (dim[1],dim[0]), interpolation = inter)
 
-    def show(self,view=(85, 0),title='Pixel Intensity',figsize=(15,10),plot=True):
-        # create the figure
-        fig=plt.figure(figsize=figsize)
-        ax = fig.add_subplot(1, 1, 1, projection='3d')
-        ax.set_title(str(title))
-        surf= ax.plot_surface(self.xx, self.yy, self.img ,rstride=1, cstride=1, cmap=self.cmap, linewidth=0, antialiased=False)
-        ax.view_init(view[0], view[1])
-        ax.grid(False)
+	def show(self,view=(85, 0),title='Pixel Intensity',figsize=(15,10),plot=True):
+	    # create the figure
+	    fig=plt.figure(figsize=figsize)
+	    ax = fig.add_subplot(1, 1, 1, projection='3d')
+	    ax.set_title(str(title))
+	    surf= ax.plot_surface(self.xx, self.yy, self.img ,rstride=1, cstride=1, cmap=self.cmap, linewidth=0, antialiased=False)
+	    ax.view_init(view[0], view[1])
+	    ax.grid(False)
 
-        cbar= fig.colorbar(surf, shrink=0.5, aspect=5)
-        cbar.set_label("Pixel Intensity")
-        ax.set_zticks([])
-        if plot: plt.show()
+	    cbar= fig.colorbar(surf, shrink=0.5, aspect=5)
+	    cbar.set_label("Pixel Intensity")
+	    ax.set_zticks([])
+	    if plot: plt.show()
 
 def plot_hist(img,hist_density_thresh=None,show=True):
 	"""
